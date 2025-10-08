3933968d846334cf9de58716d9419af25c1020ac
Bra3ke <urte.brazenaite@gmail.com>
2025-10-08 15:11:57 +0300

    Baigiau??

--- Valiutos.cpp
+++ Valiutos.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <limits>
 using namespace std;
 
 int main() {
-    string valiutos[3] = {"GBP - Didziosios Britanijos svaras",
+    string valiutos[4] = {"EUR - Euras",
+                          "GBP - Anglijos svaras",
                           "USD - JAV doleris",
                           "INR - Indijos rupija"};
-    string trumpiniai[3] = {"GBP", "USD", "INR"};
-    double kursaiPirkti[3] = {0.86, 1.1460, 101.3862};     // is euro
-    double kursaiParduoti[3] = {1/0.9220, 1.2340, 107.8546}; // i eurą
+    string trumpiniai[4] = {"EUR", "GBP", "USD", "INR"};
+    double kursaiPirkti[4] = {1.0, 0.86, 1.1460, 101.3862};
+    double kursaiParduoti[4] = {1.0, 1/0.9220, 1.2340, 107.8546};
 
     int pasirinkimas, valiuta;
     double kiekis, rezultatas;
     bool veikia = true;
 
-    cout << fixed << setprecision(4);
+    cout << fixed << setprecision(2);
 
     while (veikia) {
         cout << "\nPasirinkite funkcija:\n";
@@ -34,23 +36,23 @@ int main() {
             cout << "\n--- Valiutos kurso palyginimas ---\n";
             cout << left << setw(40) << "Valiuta"
                  << setw(15) << "Pirkimo kursas"
-                 << setw(17) << "Pardavimo kursas" << endl;
+                 << setw(15) << "Pardavimo kursas" << endl;
             cout << string(70, '-') << endl;
 
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 4; i++) {
                 cout << left << setw(40) << valiutos[i]
                      << setw(15) << kursaiPirkti[i]
                      << setw(15) << kursaiParduoti[i] << endl;
             }
 
             cout << "\nPaspauskite bet kuri migtuka, kad gristumet atgal...\n";
-            cin.ignore();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
             cin.get();
         }
         else if (pasirinkimas == 2 || pasirinkimas == 3) {
             while (true) {
                 cout << "\nPasirinkite valiuta (arba 0 - gristi atgal):\n";
-                for (int i = 0; i < 3; i++) {
+                for (int i = 0; i < 4; i++) {
                     cout << i + 1 << " - " << valiutos[i] << endl;
                 }
                 cout << "Pasirinkimas: ";
@@ -61,7 +63,7 @@ int main() {
                     break;
                 }
 
-                if (valiuta < 1 || valiuta > 3) {
+                if (valiuta < 1 || valiuta > 4) {
                     cout << "Neteisingas pasirinkimas! Bandykite dar karta:0\n";
                     continue;
                 }
@@ -69,12 +71,11 @@ int main() {
                 cout << "Iveskite kieki: ";
                 cin >> kiekis;
 
-                if (pasirinkimas == 2) { //pirkti
+                if (pasirinkimas == 2) { // pirkti
                     rezultatas = kiekis * kursaiPirkti[valiuta - 1];
                     cout << kiekis << " EUR = " << rezultatas << " "
                          << trumpiniai[valiuta - 1] << endl;
-                }
-                else { // parduoti
+                } else { // parduoti
                     rezultatas = kiekis / kursaiParduoti[valiuta - 1];
                     cout << kiekis << " " << trumpiniai[valiuta - 1]
                          << " = " << rezultatas << " EUR" << endl;
